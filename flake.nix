9561dbe94e27ff74d57f34c5375d2a45b1cdd0b5
Peter Tonner <peter.tonner@proton.me>
2024-05-26 15:47:03 -0400

    fix: add default template

--- flake.nix
+++ flake.nix
@@ -1,20 +1,23 @@
 {
   inputs = { };
-  outputs = inputs: {
-    templates = {
-      pp = {
-        path = ./python-poetry;
-        description = "Python poetry project development environment";
-        welcomeText = ''
-          # Python poetry project
-          Create development environment for python poetry projects.
+  outputs =
+    { self, ... }:
+    {
+      templates = {
+        pp = {
+          path = ./python-poetry;
+          description = "Python poetry project development environment";
+          welcomeText = ''
+            # Python poetry project
+            Create development environment for python poetry projects.
 
-          To create environment, run:
-          ```sh
-            nix develop --impure
-          ```
-        '';
+            To create environment, run:
+            ```sh
+              nix develop --impure
+            ```
+          '';
+        };
       };
+      templates.default = self.templates.pp;
     };
-  };
 }
